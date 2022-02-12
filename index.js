1787f8dc4e8d1a202818c7394e88eba7d882e122
imarmendes <imarmendes@gmail.com>
2022-02-12 19:21:50 -0300

    transformar a função para receber texto por parametro

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 //const transcription = require('./src/transcription');
+//transcription();
 const synthesize = require('./src/synthesize')
 
-//transcription();
+const play = document.querySelector('#play')
+play.addEventListener('click', trataDadosDeEntrada);
 
 const texto = 'Vamos lá, mais um teste, agora com paramentros e nome do arquivo.'
 
 const trataDadosDeEntrada = () => {
-  
+  const texto = document.querySelector('#textToTranscription').innerText;
+  console.log(texto);
 }
 
 synthesize(texto, 'teste2');
