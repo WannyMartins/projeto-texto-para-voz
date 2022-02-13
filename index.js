3141b15c8c4cb6084077e9f6ea3d9bc813033cb3
imarmendes <imarmendes@gmail.com>
2022-02-13 10:44:12 -0300

    mais teste para tentar fazer a aplicação funcionar na web, sem sucesso

--- index.js
+++ index.js
@@ -2,15 +2,20 @@
 //transcription();
 const synthesize = require('./src/synthesize')
 
-const play = document.querySelector('#play')
+/* const play = document.querySelector('#play')
 play.addEventListener('click', trataDadosDeEntrada);
 
 const texto = 'Vamos lá, mais um teste, agora com paramentros e nome do arquivo.'
 
 const trataDadosDeEntrada = () => {
-  const texto = document.querySelector('#textToTranscription').innerText;
+  const texto = document.querySelector('#textToTranscription').value;
   console.log(texto);
 }
+ */
 
-synthesize(texto, 'teste2');
+const texto = 'Vamos lá, mais um teste, agora com paramentros e nome do arquivo.'
 
+synthesize(texto, 'teste3');
+/* window.onload = () => {
+
+}  */
