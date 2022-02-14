6ce8fd462dfb9ebcf810de9e91302d1faccbb8d2
imarmendes <imarmendes@gmail.com>
2022-02-14 16:55:10 -0300

    commit de preparação para apresentação- resolvendo conflitos - atualiza o index.js

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-//const transcription = require('./src/transcription');
-//transcription();
-// const synthesize = require('./src/synthesize');
 
-import synthesize from './src/synthesize' 
-/* 
-const play = document.querySelector('#play')
-play.addEventListener('click', trataDadosDeEntrada);*/
+const synthesize = require('./src/synthesize');
 
 const test =  'Olá, somos alunos da tribo assim que se coda, e estamos muito felizes em apresentar a vocês em primeira mão a versão 1 do nosso projeto. produzido por: Caio Costa, Imar Mendes, Thiago Zardo, Wanny martins';
 synthesize(test, 'boasVindas'); 
